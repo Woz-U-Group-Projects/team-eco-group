bff2344aa9ca44ebfd190cccce8d92f93d72c222
Marco Chavez <unsettlesea@gmail.com>
2019-07-01 17:25:51 -0700

    I am changing some directories
    I am changing some directories so we can clean up components and screens.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = { users: [] };
-
-  componentDidMount(){
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-  render(){
-    return (
-      <div className="App">
-        <h1>Users: </h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{ user.username }</li>
-          )}
-        </ul>
-      </div>
-    );
+  //this component can have state
+  //This component will house our navigation and routing.
+  render() {
+    return(
+      <div>Placeholder</div>
+    ); 
   }
 }
 
