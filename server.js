18d144d3103cbb34f9223907863c1baaa467ab5d
Marco Chavez <unsettlesea@gmail.com>
2019-07-01 21:03:37 -0700

    Changing the project so it actually works
    Messed up the first time. WIll work a lot better now,

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express'); //including express in our project
+
+const app = express(); //initializing express so it's easier to use in our file
+
+const port = 5000; //declaring the port for our server to listen to requests on
+
+//this will house the logic for our requests and responses
+app.get('/api/v1', (req, res) => {
+
+    //Here is where we could include the logic for the API like receiving and authenticating login info.
+    const customers = [
+        {id: 1, firstName: 'John', lastName: 'Doe'},
+        {id: 2, firstName: 'Steve', lastName: 'Smith'},
+        {id: 3, firstName: 'Mary', lastName: 'Joe'}
+    ];
+
+    res.json(customers); //this responds with our customer object in JSON
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`)); 
+//this echo's the ports to let you know where our project is running
