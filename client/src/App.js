e191e96dd68b759105698199b121d09de01c8704
RockyWON <diz4dj@yahoo.com>
2019-07-12 17:55:18 -0400

    added axios

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Body from './components/body/Body';
+import Axios1 from './components/Axios1';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar />,
+      <Axios1 />
     </div>
   );
 }
