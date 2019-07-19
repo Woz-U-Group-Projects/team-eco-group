c3dd01a84ad2748d771fd5d53b3b6b84bae607bc
RockyWON <diz4dj@yahoo.com>
2019-07-19 18:13:01 -0400

    page changes

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import Logo2 from '../images/reduce.jpeg';
+import Logo3 from '../images/oilyPlican.jpg';
+import Logo4 from '../images/surfDebris1.jpg';
+import './home.css';
 
-const Home = () => (
-    <p>This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.This is your oceans.</p>
-);
+const Home = () => (     
+<div>
+    <img style={{width: 1100 ,height: 200}}src={Logo2}/>
+<h1>Reduce, Reuse, Recycle</h1>
+<p>Learn how reducing, reusing, and recycling can help you, your community, and the environment by saving money, energy, and natural resources. Recycling programs are managed at the state and local level—find information on recycling in your community.</p> 
+<hr />
+<div className="row">
+<h3 className="picture1">Reduce and Reuse</h3>
+<h3 className="picture">Whats Next?</h3>
+</div>
+<div className="row">
+<img style={{width: 400, height: 300}}src ={Logo3}/>
+<img style={{width: 400, height: 300}}src ={Logo4}/>
+</div>
+<div className="row" >
+    
+<ul className="row2">
+    <li><a href="https://www.epa.gov/recycle/reducing-and-reusing-basics">Reduce and reuse basics and benefits.</a></li>
+<li><a href="https://www.epa.gov/recycle/reducing-and-reusing-basics#donate">Reuse by donating your stuff</a></li>
+    </ul>
+    <ul><li><a href="https://www.epa.gov/recycle/recycling-basics">Recycling basics and benefits</a></li>
+    <li><a href="https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables">How do I recycle...</a> </li>
+    <li><a href="https://www.epa.gov/recycle/frequent-questions-recycling">Frequent Questions on Recycling</a></li>
+    <li><a href="https://www.epa.gov/recycle/america-recycles-day">America Recycles Day</a></li>
+    </ul>
+</div>
+</div>);
 
 export default Home;
--- client/src/screens/Reasons.js
+++ client/src/screens/Reasons.js
@@ -1,23 +1,29 @@
 import React from 'react';
+import './Reasons.css';
+import Logo1 from '../images/bottles.jpeg'
 
 const Reasons = () => (
     <div className="Reasons">
         <h1 className="App-title">Reasons To Recycle</h1>
 
         <h2>Reasons why I recycle and <b>You</b> should too!</h2>
-
-        <p className="left">
+        <img style={{width:1100,height:300}}src={Logo1}/>
+        <h6>
             We will eventually run out of raw materials, <br />
             so recycling keeps us from running out sooner.<br />
             Avoids wasting precious space and time dealing with <br />
-            where to put the wasted materials.</p>
+            where to put the wasted materials.</h6>
         <hr />
+        <h4>More Recycling Information</h4>
+        
+        <br />
         <p>Saves energy because less energy is used to manufacture brand new products </p>
         <hr />
         <p>Produces less greenhouse gases because industries burn fewer fossil fuels </p>
         <hr />
         <p>Recycling programs cost less than waste disposal programs </p>
         <hr />
+    <p className="bottom"></p>
     </div>
 );
 
