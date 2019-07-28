d39da99ce5c480dfc17fa43023f57e7202ae550e
elishafillis <elishafillis@my.scitexas.edu>
2019-07-28 13:51:17 -0700

    dangers link again

--- client/src/screens/Reasons.js
+++ client/src/screens/Reasons.js
@@ -24,6 +24,28 @@ const Reasons = () => (
         <p>Recycling programs cost less than waste disposal programs </p>
         <hr />
     <p className="bottom"></p>
+
+        
+    <h1>Dangers of Not Recycling Things:</h1>
+ 
+    <li><a href="https://sciencing.com/effects-not-recycling-4827369.html">Scientific dangers to society when you don't recycle.</a></li>
+    <li><a href="https://healthyseas.org/marine-protection/">fishnets affecting marine life </a></li>
+    <li><a href="https://www.epa.gov/hw/household-hazardous-waste-hhw">Household Hazards</a> </li>
+    <li><a href="https://www.epa.gov/recycle/managing-reusing-and-recycling-used-oil">Recycle your used Motor oil people!</a></li>
+    <li><a href="https://www.gacovinolake.com/news/properly-disposing-paint-consequences-following-guidelines/">Paint </a></li>
+    <li><a href="https://sciencing.com/effects-not-recycling-4827369.html">Cleaners </a></li>
+    <li><a href="https://www.hazardouswasteexperts.com/properlydisposingpesticides/">Pesticides </a></li>
+    <li><a href="https://www.maxsainnovations.com/blog/the-danger-of-not-recycling-batteries/">batteries </a></li>
+    <li><a href="http://astorbridgemarina.blogspot.com/2011/08/be-hippie-for-day.html">old clothes </a></li>
+    
+<br></br>
+    
+    <h1>Dangers of Global industrialization:</h1>
+
+    <li><a href="https://sites.google.com/site/5effectsofindustrialization/effects-in-the-1800s-1900s">affects of global industrialization.</a></li>
+    <li><a href="https://www.sciencedirect.com/science/article/pii/S1352231011000070">lower crop yeilds.</a></li>
+    <li><a href="https://www.coolearth.org/2018/10/ipcc-report-2/?gclid=Cj0KCQjw1MXpBRDjARIsAHtdN-0U7QqSlN_UEYEVejAKZ_RLxxoPyIRM6Ij5v6fFkOgcwCY21d9ANVgaAjRBEALw_wcB">Global Warming</a></li>
+
     </div>
 );
 
