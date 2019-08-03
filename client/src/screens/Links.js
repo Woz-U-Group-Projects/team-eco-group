53802e709300612301dceb6f256b921ecfe46324
elishafillis <elishafillis@my.scitexas.edu>
2019-08-03 12:50:42 -0700

    updated link.js css

--- client/src/screens/Links.js
+++ client/src/screens/Links.js
@@ -1,44 +1,43 @@
 import React from 'react';
 import './links.css';
-import Logo5 from '../images/body-background.jpg';
+// import Logo5 from '../images/sufDebris1.jpg';
+    {/* <img style={{width: 800, height: 400}}src={Logo5}/>   */}
 
 const Links = () => (
+        
+    <div className="linksImage" >
+    <div class="grid">
+        <div class="col-2-3">
+            <article class="module">                
+                <h3>Resource Links to Dangers</h3>
+                <h1>Dangers of Not Recycling Things:</h1>
+                    <ul>
+                        <li><a id="topic-link" href="https://sciencing.com/effects-not-recycling-4827369.html">Scientific dangers to society when you don't recycle.</a></li>
+                        <li><a id="topic-link" href="https://healthyseas.org/marine-protection/">fishnets affecting marine life </a> </li>
+                        <li><a id="topic-link" href="https://www.epa.gov/hw/household-hazardous-waste-hhw">Household Hazards</a></li>
+                        <li><a id="topic-link" href="https://www.epa.gov/recycle/managing-reusing-and-recycling-used-oil">Recycle your used Motor oil</a></li>
+                        <li><a id="topic-link" href="https://www.gacovinolake.com/news/properly-disposing-paint-consequences-following-guidelines/">Paint </a></li>
+                        <li><a id="topic-link" href="https://sciencing.com/effects-not-recycling-4827369.html">Cleaners </a></li>
+                        <li><a id="topic-link" href="https://www.hazardouswasteexperts.com/properlydisposingpesticides/">Pesticides </a></li>
+                        <li><a id="topic-link" href="https://www.maxsainnovations.com/blog/the-danger-of-not-recycling-batteries/">batteries </a></li>
+                        </ul>
+            </article>
+        </div>
 
-<div>
-    <h3>Resource Links to Dangers</h3>
-    <img style={{width: 800 ,height: 400}}src={Logo5}/>
+        <div class="col-1-3">
+            <aside class="module">
+                <ul>
+                    <h1>Dangers of Global industrialization:</h1>
+                    <li><a id="topic-link" href="https://sites.google.com/site/5effectsofindustrialization/effects-in-the-1800s-1900s">affects of global industrialization.</a></li>
+                    <li><a id="topic-link" href="https://www.sciencedirect.com/science/article/pii/S1352231011000070">lower crop yeilds.</a></li>
+                    <li><a id="topic-link" href="https://www.coolearth.org/2018/10/ipcc-report-2/?gclid=Cj0KCQjw1MXpBRDjARIsAHtdN-0U7QqSlN_UEYEVejAKZ_RLxxoPyIRM6Ij5v6fFkOgcwCY21d9ANVgaAjRBEALw_wcB">Global Warming</a></li>
+                </ul>
+            </aside>
+        </div>
 
-    <div>
-    
-    <h1>Dangers of Not Recycling Things:</h1>
 
-    <div className="row">
-   <ul>
-    <li><a id="topic-link" href="https://sciencing.com/effects-not-recycling-4827369.html">Scientific dangers to society when you don't recycle.</a></li>
-    <li><a id="topic-link" href="https://healthyseas.org/marine-protection/">fishnets affecting marine life </a> </li>
-    <li><a id="topic-link" href="https://www.epa.gov/hw/household-hazardous-waste-hhw">Household Hazards</a></li>
-    <li><a id="topic-link" href="https://www.epa.gov/recycle/managing-reusing-and-recycling-used-oil">Recycle your used Motor oil</a></li>
-    <li><a id="topic-link" href="https://www.gacovinolake.com/news/properly-disposing-paint-consequences-following-guidelines/">Paint </a></li>
-    <li><a id="topic-link" href="https://sciencing.com/effects-not-recycling-4827369.html">Cleaners </a></li>
-    <li><a id="topic-link" href="https://www.hazardouswasteexperts.com/properlydisposingpesticides/">Pesticides </a></li>
-    <li><a id="topic-link" href="https://www.maxsainnovations.com/blog/the-danger-of-not-recycling-batteries/">batteries </a></li>
-    </ul>
-    </div>
-
-    <div>
-    <div className="row">
-    <ul className="row2">
-        <h1>Dangers of Global industrialization:</h1>
-        <li><a id="topic-link" href="https://sites.google.com/site/5effectsofindustrialization/effects-in-the-1800s-1900s">affects of global industrialization.</a></li>
-        <li><a id="topic-link" href="https://www.sciencedirect.com/science/article/pii/S1352231011000070">lower crop yeilds.</a></li>
-        <li><a id="topic-link" href="https://www.coolearth.org/2018/10/ipcc-report-2/?gclid=Cj0KCQjw1MXpBRDjARIsAHtdN-0U7QqSlN_UEYEVejAKZ_RLxxoPyIRM6Ij5v6fFkOgcwCY21d9ANVgaAjRBEALw_wcB">Global Warming</a></li>
-    </ul>
-    </div>
-    </div>
-   
 </div>
-
-</div>  
+</div> 
 );
 
 export default Links;
